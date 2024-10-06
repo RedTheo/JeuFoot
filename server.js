56d2c02bcc82a6de3068e6c0b2030f1f86beecfa
Benoit D <benoit.dessaint2003@gmail.com>
2024-10-06 23:37:51 +0200

    physique de la balle, textures , gestion de score et des équipes

--- server.js
+++ server.js
@@ -14,54 +14,121 @@ const io = socketIo(server);
 
 // Utilisation d'Express pour servir les fichiers statiques (HTML, CSS, JS) situés dans le dossier "public"
 app.use(express.static(__dirname + '/public'));
+app.use(express.static("."));
 
 // Variable pour stocker les joueurs connectés, chaque joueur sera identifié par un ID unique
 let players = {};
+let hostPlayerId = null; 
+let teamRedCount = 0;
+let teamBlueCount = 0;
+// Variables pour stocker les scores des équipes
+let scores = { teamRed: 0, teamBlue: 0 };
+
+
+
+// Variable pour gérer la balle
+let ball = {
+    x: 0,
+    y: 0,
+    z: 0,
+    velocity: { x: 0, y: 0, z: 0 }
+};
 
 // Gestion des événements lorsqu'un utilisateur se connecte au serveur
 io.on('connection', (socket) => {
- console.log('Nouvel utilisateur connecté :', socket.id);
 
- // Lorsqu'un nouveau joueur se connecte, on lui attribue un cube dans l'espace 3D.
- // Les coordonnées X et Z sont générées aléatoirement pour placer chaque joueur à un endroit différent.
- // Chaque joueur a aussi une couleur aléatoire.
- players[socket.id] = {
- id: socket.id, // ID unique pour identifier le joueur
- x: Math.random() * 10 - 5, // Position X du joueur (aléatoire)
- y: 0, // Position Y (0 car les joueurs sont sur le sol)
- z: Math.random() * 10 - 5, // Position Z du joueur (aléatoire)
- color: '#' + Math.floor(Math.random() * 16777215).toString(16) // Couleur aléatoire
- };
+    teamRedCount = Object.values(players).filter(player => player.team === 'teamRed').length;
+    teamBlueCount = Object.values(players).filter(player => player.team === 'teamBlue').length;
+    console.log(teamRedCount);
+    console.log(teamBlueCount);
+    console.log('Nouvel utilisateur connecté :', socket.id);
+    if (!hostPlayerId) {
+        hostPlayerId = socket.id;
+        console.log(`Joueur ${socket.id} est désigné comme l'hôte`);
+        io.to(socket.id).emit('hostAssigned'); // Envoyer un événement au client pour lui dire qu'il est l'hôte
+    }
+    io.emit('hostUpdate', { hostId: hostPlayerId });
 
- // Envoyer la liste de tous les joueurs actuels au nouveau joueur
- socket.emit('init', players);
+    socket.on('disconnect', () => {
+        console.log(`Joueur déconnecté : ${socket.id}`);
+        if (socket.id === hostPlayerId) {
+            const connectedPlayers = Object.keys(io.sockets.sockets);
+            if (connectedPlayers.length > 0) {
+                hostPlayerId = connectedPlayers[0];
+                io.to(hostPlayerId).emit('hostAssigned'); 
+                io.emit('hostUpdate', { hostId: hostPlayerId }); 
+            } else {
+                hostPlayerId = null; 
+            }
+        }
+    });
+    // Attribuer le joueur à une équipe aléatoire (Rouge ou Bleu)
+    const team = teamRedCount <= teamBlueCount ? 'teamRed' : 'teamBlue';
+    let positionX;
+    if (team === 'teamRed') {
+        // Position pour les joueurs rouges (droite du terrain)
+        positionX = Math.random() * 10 + 1; // entre 0 et 5
+    } else {
+        // Position pour les joueurs bleus (gauche du terrain)
+        positionX = Math.random() * -10 -1; // entre -5 et 0
+    }
 
- // Informer tous les autres joueurs qu'un nouveau joueur vient de se connecter
- socket.broadcast.emit('newPlayer', players[socket.id]);
+    // Assigner au joueur une position aléatoire et une couleur en fonction de son équipe
+    players[socket.id] = {
+        id: socket.id, 
+        x: positionX,
+        y: 0, 
+        z: Math.random() * 5 - 5, 
+        color: team === 'teamRed' ? '#FF0000' : '#0000FF', // Rouge ou Bleu en fonction de l'équipe
+        team: team // Stocker l'équipe du joueur
+    };
 
- // Lorsqu'un joueur se déplace, il envoie sa nouvelle position au serveur
- socket.on('move', (data) => {
- // Mettre à jour la position du joueur dans la liste des joueurs
- players[socket.id].x = data.x;
- players[socket.id].z = data.z;
+    // Envoyer la position de la balle et les scores actuels au nouveau joueur
+    socket.emit('ballPosition', ball);
+    socket.emit('scoreUpdate', scores);
 
- // Envoyer les nouvelles coordonnées du joueur à tous les autres joueurs connectés
- socket.broadcast.emit('playerMoved', players[socket.id]);
- });
+    // Envoyer la liste de tous les joueurs actuels au nouveau joueur
+    socket.emit('init', players);
 
- // Gestion de la déconnexion d'un joueur
- socket.on('disconnect', () => {
- console.log('Utilisateur déconnecté :', socket.id);
+    // Informer tous les autres joueurs qu'un nouveau joueur vient de se connecter
+    socket.broadcast.emit('newPlayer', players[socket.id]);
 
- // Retirer le joueur de la liste des joueurs connectés
- delete players[socket.id];
+    // Recevoir les mises à jour de la balle depuis un joueur
+    socket.on('ballMoved', (newBall) => {
+        
+        socket.broadcast.emit('ballPosition', newBall);
+    });
 
- // Informer tous les autres joueurs qu'un joueur s'est déconnecté
- io.emit('playerDisconnected', socket.id);
- });
+    // Lorsqu'un joueur se déplace, il envoie sa nouvelle position au serveur
+    socket.on('move', (data) => {
+        players[socket.id].x = data.x;
+        players[socket.id].z = data.z;
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+    // Écouter les mises à jour de score
+    socket.on('scoreUpdate', (data) => {
+        if (data.team === 'blue') {
+            scores.teamBlue++;
+            console.log("But bleu");
+        } else if (data.team === 'red') {
+            scores.teamRed++;
+            console.log("But rouge");
+        }
+
+        // Émettre les scores mis à jour à tous les clients
+        io.emit('scoreUpdate', scores);
+    });
+    // Gestion de la déconnexion d'un joueur
+    socket.on('disconnect', () => {
+        console.log('Utilisateur déconnecté :', socket.id);
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+
+    
 });
 
 // Le serveur écoute les connexions sur le port 3000
 server.listen(3000, () => {
- console.log('Serveur démarré sur le port 3000');
+    console.log('Serveur démarré sur le port 3000');
 });
